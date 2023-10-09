d0612eaa3cba6ee34bf739732f1b8e36cc166047
rabby-shek <rabbyshek1998@gmail.com>
2023-10-09 13:22:13 +0600

    translation.js added

---
+++ translation.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggle-language");
+    const elementsToTranslate = document.querySelectorAll("[data-translate]");
+    let isEnglish = true;
+
+    function toggleLanguage() {
+        isEnglish = !isEnglish; // Toggle between English and Bengali
+
+        elementsToTranslate.forEach(element => {
+            const translationKey = element.getAttribute("data-translate");
+            const translation = isEnglish ? element.getAttribute("data-english") : element.getAttribute("data-bengali");
+
+            if (translation) {
+                element.textContent = translation;
+            }
+        });
+
+        // Set session storage to remember the current language choice
+        sessionStorage.setItem("language", isEnglish ? "english" : "bengali");
+    }
+
+    toggleButton.addEventListener("click", toggleLanguage);
+
+    // Check session storage for the previously selected language
+    const storedLanguage = sessionStorage.getItem("language");
+    if (storedLanguage === "bengali") {
+        toggleLanguage(); // Switch to Bengali if it was previously selected
+    }
+});
